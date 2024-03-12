3399dc0cefd94ec5bd48f75ff807642e37a7ce40
Darakhshan Fazail <darakhshanfazail@sarfarazs-air.wkstn.qmul.ac.uk>
2024-03-12 19:49:34 +0000

    Add media folder and MEDIA_URL with MEDIA_ROOT

--- myecomm/myecomm/settings.py
+++ myecomm/myecomm/settings.py
@@ -119,9 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "static/" # URL prefix or endpoint to access the files on web
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static") # file system path or directory where files are stored
+
+STATICFILES_DIR = os.path.join(BASE_DIR, "staticfiles")
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
